cf2237dfd1fe731b60668f339fe733298c2806c9
Smoodle <migwwe@gmail.com>
2020-08-03 01:05:33 +0100

    Changed background colors

--- config.h
+++ config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font Mono:size=12:antialias=true:autohint=true";
+static char *font = "mononoki Nerd Font Mono:size=14:antialias=true:autohint=true";
+//static char *font = "lucy tewihm:style=Regular:fontsize=9";
 static int borderpx = 2;
 
 /*
@@ -143,6 +144,14 @@ static const char *colorname[] = {
   [13] = "#d3869b", /* magenta */
   [14] = "#8ec07c", /* cyan    */
   [15] = "#ebdbb2", /* white   */
+
+  [255] = 0,
+
+  /* more colors can be added after 255 to use with DefaultXX */
+  "#add8e6", /* 256 -> cursor */
+  "#555555", /* 257 -> rev cursor*/
+  "#282828", /* 258 -> bg */
+  "#ebdbb2", /* 259 -> fg */
 };
 
 
@@ -150,15 +159,20 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-//unsigned int defaultfg = 257;
-//unsigned int defaultbg = 256;
-//static unsigned int defaultcs = 257;
-//static unsigned int defaultrcs = 256;
+//unsigned int defaultfg = 7;
+//unsigned int defaultbg = 0;
+//static unsigned int defaultcs = 256;
+//static unsigned int defaultrcs = 257;
 
-unsigned int defaultfg = 15;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 15;
-static unsigned int defaultrcs = 256;
+//unsigned int defaultfg = 257;
+//unsigned int defaultbg = 258;
+//static unsigned int defaultcs = 256;
+//static unsigned int defaultrcs = 257;
+
+unsigned int defaultfg = 259;
+unsigned int defaultbg = 258;
+unsigned int defaultcs = 256;
+unsigned int defaultrcs = 257;
 
 /*
  * Default shape of cursor
@@ -217,9 +231,9 @@ ResourcePref resources[] = {
 		{ "color13",      STRING,  &colorname[13] },
 		{ "color14",      STRING,  &colorname[14] },
 		{ "color15",      STRING,  &colorname[15] },
-		{ "background",   STRING,  &colorname[256] },
-		{ "foreground",   STRING,  &colorname[257] },
-		{ "cursorColor",  STRING,  &colorname[258] },
+		{ "background",   STRING,  &colorname[258] },
+		{ "foreground",   STRING,  &colorname[259] },
+		{ "cursorColor",  STRING,  &colorname[256] },
 		{ "termname",     STRING,  &termname },
 		{ "shell",        STRING,  &shell },
 		{ "blinktimeout", INTEGER, &blinktimeout },
